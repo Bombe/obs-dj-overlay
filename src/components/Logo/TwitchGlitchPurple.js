dec5af42f68560364d9b8190ffd431a836cf8570
David ‘Bombe’ Roden <bombe@pterodactylus.net>
2020-06-06 00:13:08 +0200

    ♿️ Add alt text for Twitch logo

--- src/components/Logo/TwitchGlitchPurple.js
+++ src/components/Logo/TwitchGlitchPurple.js
@@ -2,6 +2,6 @@ import React from "react";
 import logo from "../../assets/TwitchGlitchPurple.svg";
 
 const TwitchGlitchPurple = ({size}) =>
-    <img src={logo} style={{width: size, height: size}}/>
+    <img src={logo} alt="Twitch Logo" style={{width: size, height: size}}/>
 
 export default TwitchGlitchPurple;
