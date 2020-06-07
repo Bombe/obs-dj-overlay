a8fad8b1d0a91c446c40e46e5aa90003dff90ae6
David ‘Bombe’ Roden <bombe@pterodactylus.net>
2020-06-07 16:05:20 +0200

    🐛 Improve measurement of text

--- src/components/Message/Message.js
+++ src/components/Message/Message.js
@@ -11,22 +11,27 @@ const Message = () => {
     const overlayInfo = useContext(OverlayContext)
 
     useEffect(() => {
-        const containerElement = document.querySelector(".Message");
-        const containerStyle = window.getComputedStyle(containerElement);
-        const containerWidth = fromPx(containerStyle.width);
-        const containerHeight = fromPx(containerStyle.height);
-        const textElement = containerElement.querySelector(".Text");
-        const textStyle = window.getComputedStyle(textElement);
-        const textWidth = fromPx(textStyle.width);
-        const textHeight = fromPx(textStyle.height);
-        const scale = Math.min(containerWidth / textWidth, containerHeight / textHeight);
-        if (isFinite(scale)) {
-            textElement.setAttribute("style", "font-size: " + Math.floor(scale * 100) + "%;");
+        if (overlayInfo.message) {
+            const containerElement = document.querySelector(".Message");
+            const containerStyle = window.getComputedStyle(containerElement);
+            const containerWidth = fromPx(containerStyle.width);
+            const containerHeight = fromPx(containerStyle.height);
+            const textElement = containerElement.querySelector(".Measurement");
+            const textStyle = window.getComputedStyle(textElement);
+            const textWidth = fromPx(textStyle.width);
+            const textHeight = fromPx(textStyle.height);
+            const scale = Math.min(containerWidth / textWidth, containerHeight / textHeight);
+            if (isFinite(scale)) {
+                containerElement
+                    .querySelector(".Text")
+                    .setAttribute("style", "font-size: " + Math.floor(scale * 100) + "%;");
+            }
         }
     }, [overlayInfo.message]);
 
     return (
         <div className="Message">
+            <div className="Measurement">{overlayInfo.message}</div>
             <div className="Text">{overlayInfo.message}</div>
         </div>
     );
