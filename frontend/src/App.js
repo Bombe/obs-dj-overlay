2001c3b00683897eeac84aa0895ddbcee85f6d6a
David ‘Bombe’ Roden <bombe@pterodactylus.net>
2020-06-08 21:06:08 +0200

    🎨 Add router to main app page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,18 @@
 import React from "react"
 
 import Viewer from "./components/Viewer"
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom"
 
 const App = () => {
-    return (<Viewer/>)
+    return (
+        <Router>
+            <Switch>
+                <Route path="/">
+                    <Viewer/>
+                </Route>
+            </Switch>
+        </Router>
+    )
 }
 
 export default App
