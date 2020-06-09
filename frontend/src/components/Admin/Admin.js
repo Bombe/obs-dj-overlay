03dc95d128dc15f1a49683012bd23ff7266b12a5
David ‘Bombe’ Roden <bombe@pterodactylus.net>
2020-06-09 23:06:00 +0200

    🚸 Auto-increment track number on track data change

--- frontend/src/components/Admin/Admin.js
+++ frontend/src/components/Admin/Admin.js
@@ -58,7 +58,15 @@ const Admin = () => {
     }
 
     const sendTrackInfo = (event) => {
-        overlayService.setTrackInfo(trackNumber, trackArtist, trackTitle)
+        overlayService.get()
+            .then(overlayInfo => {
+                if (((overlayInfo.track.artist !== trackArtist) || (overlayInfo.track.title !== trackTitle)) && trackNumber && (overlayInfo.track.number === trackNumber)) {
+                    setTrackNumber(trackNumber + 1)
+                    overlayService.setTrackInfo(trackNumber + 1, trackArtist, trackTitle)
+                } else {
+                    overlayService.setTrackInfo(trackNumber, trackArtist, trackTitle)
+                }
+            })
         event.preventDefault()
     }
 
