95429be889e6179d0569a0e29d975574bc4965b8
David ‘Bombe’ Roden <bombe@pterodactylus.net>
2020-06-10 21:02:53 +0200

    ✨ Add preview, stream, and chat

--- frontend/src/components/Admin/Admin.js
+++ frontend/src/components/Admin/Admin.js
@@ -1,6 +1,10 @@
+import Box from "@material-ui/core/Box"
+import Paper from "@material-ui/core/Paper"
 import React, {useEffect, useState} from "react"
 import Grid from "@material-ui/core/Grid"
 import Typography from "@material-ui/core/Typography"
+import SplitPane from "react-split-pane"
+import {TwitchChat, TwitchPlayer} from "react-twitch-embed"
 
 import overlayService from "../../services/overlay"
 
@@ -9,6 +13,7 @@ import ShowAdmin from "./show"
 import TrackAdmin from "./track"
 import TwitchAdmin from "./twitch"
 import styles from "./Admin.module.css"
+import "./Resizer.css"
 
 const Admin = () => {
 
@@ -86,26 +91,45 @@ const Admin = () => {
             })
     }, [])
 
+    const setPlayerHeight = (size) => {
+        const playerStyle = document.getElementById("twitch-video-player").style
+        playerStyle.width = size.toString() + "px"
+        playerStyle.height = (size * 9 / 16).toString() + "px"
+        const previewStyle = document.getElementById("preview-frame").style
+        previewStyle.width = size.toString() + "px"
+        previewStyle.height = (size * 9/16).toString() + "px"
+        document.getElementById("preview-iframe").style.transform = "scale("+(size/1280)+")"
+    }
+
     return (
-        <div className={styles.Admin}>
-            <Typography variant="h3">Admin Interface</Typography>
-            <Grid className={styles.Inputs} container spacing={3}>
-                <Grid item xs={12} sm={6} lg={4} xl={3}>
-                    <ShowAdmin showTitle={showTitle} showSubtitle={showSubtitle} nextShow={nextShow} setShowTitle={setShowTitle} setShowSubtitle={setShowSubtitle} setNextShow={setNextShow}
-                               sendShowInfo={sendShowInfo} originalShowTitle={originalShowTitle} originalShowSubtitle={originalShowSubtitle} originalNextShow={originalNextShow}/>
+        <SplitPane split={"vertical"} primary={"second"} minSize={400} allowResize={true} onChange={setPlayerHeight}>
+            <Paper className={styles.Admin}>
+                <Typography variant="h3">Admin Interface</Typography>
+                <Grid className={styles.Inputs} container spacing={3}>
+                    <Grid item xs={12} sm={6} lg={4}>
+                        <ShowAdmin showTitle={showTitle} showSubtitle={showSubtitle} nextShow={nextShow} setShowTitle={setShowTitle} setShowSubtitle={setShowSubtitle} setNextShow={setNextShow}
+                                   sendShowInfo={sendShowInfo} originalShowTitle={originalShowTitle} originalShowSubtitle={originalShowSubtitle} originalNextShow={originalNextShow}/>
+                    </Grid>
+                    <Grid item xs={12} sm={6} lg={4}>
+                        <TrackAdmin number={trackNumber} artist={trackArtist} title={trackTitle} setNumber={setFilteredTrackNumber} setArtist={setTrackArtist} setTitle={setTrackTitle}
+                                    originalNumber={originalTrackNumber} originalArtist={originalTrackArtist} originalTitle={originalTrackTitle} sendTrackInfo={sendTrackInfo}/>
+                    </Grid>
+                    <Grid item xs={12} sm={6} lg={4}>
+                        <MessageAdmin message={message} setMessage={setMessage} originalMessage={originalMessage} sendMessage={sendMessage}/>
+                    </Grid>
+                    <Grid item xs={12} sm={6} lg={4}>
+                        <TwitchAdmin username={twitchUserName} setUsername={setTwitchUserName} originalUsername={originalTwitchUserName} sendTwitchData={sendTwitchData}/>
+                    </Grid>
                 </Grid>
-                <Grid item xs={12} sm={6} lg={4} xl={3}>
-                    <TrackAdmin number={trackNumber} artist={trackArtist} title={trackTitle} setNumber={setFilteredTrackNumber} setArtist={setTrackArtist} setTitle={setTrackTitle}
-                                originalNumber={originalTrackNumber} originalArtist={originalTrackArtist} originalTitle={originalTrackTitle} sendTrackInfo={sendTrackInfo}/>
-                </Grid>
-                <Grid item xs={12} sm={6} lg={4} xl={3}>
-                    <MessageAdmin message={message} setMessage={setMessage} originalMessage={originalMessage} sendMessage={sendMessage}/>
-                </Grid>
-                <Grid item xs={12} sm={6} lg={4} xl={3}>
-                    <TwitchAdmin username={twitchUserName} setUsername={setTwitchUserName} originalUsername={originalTwitchUserName} sendTwitchData={sendTwitchData}/>
-                </Grid>
-            </Grid>
-        </div>
+            </Paper>
+            <Box display={"flex"} flexDirection={"column"} height={"100%"}>
+                <Box id="preview-frame" height={225}>
+                    <iframe id={"preview-iframe"} style={{transform: "scale(0.3125)", transformOrigin: "0 0", overflow: "hidden"}} title={"Preview"} width={1280} height={720} src={"/"}/>
+                </Box>
+                <Box><TwitchPlayer id="twitch-video-player" channel="bombe___" width={400} height={225}/></Box>
+                <Box flexGrow={1} id="twitch-chat-box"><TwitchChat id="twitch-chat" channel="bombe___" parent={["localhost"]} width="100%" height="100%"/></Box>
+            </Box>
+        </SplitPane>
     )
 }
 
