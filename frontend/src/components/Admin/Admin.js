ffff675e65cde06629471f328a6ac04a4879e987
David ‘Bombe’ Roden <bombe@pterodactylus.net>
2020-07-10 14:39:04 +0200

    ✨ Add embeds for all the admin sections

--- frontend/src/components/Admin/Admin.js
+++ frontend/src/components/Admin/Admin.js
@@ -1,31 +1,57 @@
 import React from "react"
 import Box from "@material-ui/core/Box"
 import {TwitchEmbed} from "react-twitch-embed"
+import {BrowserRouter as Router, Route, Switch, useRouteMatch} from "react-router-dom"
 
 import AdminSection from "./main"
 import styles from "./Admin.module.css"
 import config from "../../utils/config"
+import {MessageAdmin} from "./message/MessageAdmin"
+import {ShowAdmin} from "./show/ShowAdmin"
+import {TrackAdmin} from "./track/TrackAdmin"
+import {TwitchAdmin} from "./twitch/TwitchAdmin"
 
 const Admin = () => {
 
     const twitchChannel = config.twitch.channel
 
+    const {path} = useRouteMatch()
+
     return (
-        <div className={styles.Admin}>
-            <Box className={styles.Screen} display="flex" flexDirection="row">
-                <Box className={styles.Left} flexGrow={1}>
-                    <AdminSection/>
-                </Box>
-                <Box className={styles.Right}>
-                    <Box display="flex" flexDirection="column" height="100%">
-                        <Box id="preview-frame" height={225}>
-                            <iframe id="preview-iframe" className={styles.Preview} style={{transform: "scale(0.3125)", transformOrigin: "0 0", overflow: "hidden"}} title="Preview" width={1280} height={720} src={"/preview"}/>
+        <Router>
+            <Switch>
+                <Route path={`${path}/embed/show`}>
+                    <ShowAdmin/>
+                </Route>
+                <Route path={`${path}/embed/track`}>
+                    <TrackAdmin/>
+                </Route>
+                <Route path={`${path}/embed/message`}>
+                    <MessageAdmin/>
+                </Route>
+                <Route path={`${path}/embed/twitch`}>
+                    <TwitchAdmin/>
+                </Route>
+                <Route exact path={path}>
+                    <div className={styles.Admin}>
+                        <Box className={styles.Screen} display="flex" flexDirection="row">
+                            <Box className={styles.Left} flexGrow={1}>
+                                <AdminSection/>
+                            </Box>
+                            <Box className={styles.Right}>
+                                <Box display="flex" flexDirection="column" height="100%">
+                                    <Box id="preview-frame" height={315}>
+                                        <iframe id="preview-iframe" className={styles.Preview} style={{transform: "scale(0.4375)", transformOrigin: "0 0", overflow: "hidden"}} title="Preview"
+                                                width={1280} height={720} src={"/preview"}/>
+                                    </Box>
+                                    <Box flexGrow={1}><TwitchEmbed muted={true} channel={twitchChannel} parent={["localhost"]} width="100%" height="100%"/></Box>
+                                </Box>
+                            </Box>
                         </Box>
-                        <Box flexGrow={1}><TwitchEmbed muted={true} channel={twitchChannel} parent={["localhost"]} width="100%" height="100%"/></Box>
-                    </Box>
-                </Box>
-            </Box>
-        </div>
+                    </div>
+                </Route>
+            </Switch>
+        </Router>
     )
 
 }
