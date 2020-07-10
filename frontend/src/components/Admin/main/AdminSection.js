c8cd419c55de458a842022438280c7a5bb7a10a2
David ‘Bombe’ Roden <bombe@pterodactylus.net>
2020-07-10 14:20:38 +0200

    ♻️ Move track-related state to track admin

--- frontend/src/components/Admin/main/AdminSection.js
+++ frontend/src/components/Admin/main/AdminSection.js
@@ -12,31 +12,10 @@ import styles from "./AdminSection.module.css"
 
 const AdminSection = () => {
 
-    const [trackNumber, setTrackNumber] = useState(0)
-    const [trackArtist, setTrackArtist] = useState("")
-    const [trackTitle, setTrackTitle] = useState("")
-    const [originalTrackNumber, setOriginalTrackNumber] = useState(0)
-    const [originalTrackArtist, setOriginalTrackArtist] = useState("")
-    const [originalTrackTitle, setOriginalTrackTitle] = useState("")
     const [message, setMessage] = useState("")
     const [originalMessage, setOriginalMessage] = useState("")
     const [twitchUserName, setTwitchUserName] = useState("")
     const [originalTwitchUserName, setOriginalTwitchUserName] = useState("")
-
-    const setFilteredTrackNumber = (value) => {
-        setTrackNumber(parseInt(value.toString().replace(/[^0-9]/, "")) || 0)
-    }
-
-    const sendTrackInfo = (event) => {
-        overlayService.setTrackInfo(trackNumber, trackArtist, trackTitle)
-        setOriginalTrackNumber(trackNumber)
-        setOriginalTrackArtist(trackArtist)
-        setOriginalTrackTitle(trackTitle)
-        if (trackNumber) {
-            setTrackNumber(trackNumber + 1)
-        }
-        event.preventDefault()
-    }
 
     const sendMessage = (event) => {
         overlayService.setMessage(message)
@@ -53,12 +32,6 @@ const AdminSection = () => {
     useEffect(() => {
         overlayService.get()
             .then(overlayInfo => {
-                setTrackNumber(overlayInfo.track.number)
-                setTrackArtist(overlayInfo.track.artist)
-                setTrackTitle(overlayInfo.track.title)
-                setOriginalTrackNumber(overlayInfo.track.number)
-                setOriginalTrackArtist(overlayInfo.track.artist)
-                setOriginalTrackTitle(overlayInfo.track.title)
                 setMessage(overlayInfo.message)
                 setOriginalMessage(overlayInfo.message)
                 setTwitchUserName(overlayInfo.twitchUserName)
@@ -74,8 +47,7 @@ const AdminSection = () => {
                     <ShowAdmin/>
                 </Grid>
                 <Grid item xs={12} sm={6} lg={4}>
-                    <TrackAdmin number={trackNumber} artist={trackArtist} title={trackTitle} setNumber={setFilteredTrackNumber} setArtist={setTrackArtist} setTitle={setTrackTitle}
-                                originalNumber={originalTrackNumber} originalArtist={originalTrackArtist} originalTitle={originalTrackTitle} sendTrackInfo={sendTrackInfo}/>
+                    <TrackAdmin/>
                 </Grid>
                 <Grid item xs={12} sm={6} lg={4}>
                     <MessageAdmin message={message} setMessage={setMessage} originalMessage={originalMessage} sendMessage={sendMessage}/>
--- frontend/src/components/Admin/track/TrackAdmin.js
+++ frontend/src/components/Admin/track/TrackAdmin.js
@@ -1,30 +1,65 @@
 import {DoneAll, Undo} from "@material-ui/icons"
-import React from "react"
+import React, {useEffect, useState} from "react"
 import Box from "@material-ui/core/Box"
 import Button from "@material-ui/core/Button"
 import Grid from "@material-ui/core/Grid"
 
 import {blur, onEnter, onValueEventRun} from "../../../utils/event"
 import useFocus from "../../../utils/focus"
+import overlayService from "../../../services/overlay"
 import Group from "../../Group"
 import SelectOnFocusTextField from "../../selectOnFocus"
 
-const TrackAdmin = ({number, artist, title, setNumber, setArtist, setTitle, originalNumber, originalArtist, originalTitle, sendTrackInfo}) => {
+const TrackAdmin = () => {
+
+    const [trackNumber, setTrackNumber] = useState(0)
+    const [trackArtist, setTrackArtist] = useState("")
+    const [trackTitle, setTrackTitle] = useState("")
+    const [originalTrackNumber, setOriginalTrackNumber] = useState(0)
+    const [originalTrackArtist, setOriginalTrackArtist] = useState("")
+    const [originalTrackTitle, setOriginalTrackTitle] = useState("")
+
+    const setFilteredTrackNumber = (value) => {
+        setTrackNumber(parseInt(value.toString().replace(/[^0-9]/, "")) || 0)
+    }
+
+    const sendTrackInfo = (event) => {
+        overlayService.setTrackInfo(trackNumber, trackArtist, trackTitle)
+        setOriginalTrackNumber(trackNumber)
+        setOriginalTrackArtist(trackArtist)
+        setOriginalTrackTitle(trackTitle)
+        if (trackNumber) {
+            setTrackNumber(trackNumber + 1)
+        }
+        event.preventDefault()
+    }
 
     const [trackArtistField, focusTrackArtist] = useFocus()
     const [trackTitleField, focusTrackTitle] = useFocus()
 
     const decrementTrackNumber = () => {
-        setNumber(number - 1)
+        setTrackNumber(trackNumber - 1)
     }
 
     const restoreTrackInfo = () => {
-        setNumber(originalNumber)
-        setArtist(originalArtist)
-        setTitle(originalTitle)
+        setTrackNumber(originalTrackNumber)
+        setTrackArtist(originalTrackArtist)
+        setTrackTitle(originalTrackTitle)
     }
 
-    const modificationsPresent = (number !== originalNumber) || (artist !== originalArtist) || (title !== originalTitle)
+    const modificationsPresent = (trackNumber !== originalTrackNumber) || (trackArtist !== originalTrackArtist) || (trackTitle !== originalTrackTitle)
+
+    useEffect(() => {
+        overlayService.get()
+            .then(overlayInfo => {
+                setTrackNumber(overlayInfo.track.number)
+                setTrackArtist(overlayInfo.track.artist)
+                setTrackTitle(overlayInfo.track.title)
+                setOriginalTrackNumber(overlayInfo.track.number)
+                setOriginalTrackArtist(overlayInfo.track.artist)
+                setOriginalTrackTitle(overlayInfo.track.title)
+            })
+    }, [])
 
     return (
         <Group title="Track">
@@ -33,8 +68,8 @@ const TrackAdmin = ({number, artist, title, setNumber, setArtist, setTitle, orig
                     <Grid item xs={12}>
                         <Box display="flex" alignItems="center">
                             <Box flexGrow={1}>
-                                <SelectOnFocusTextField label="The number of the track" variant="filled" value={number} onChange={onValueEventRun(setNumber)}
-                                                        onKeyPress={onEnter(focusTrackArtist, true)} fullWidth={true} error={number !== originalNumber} tabIndex={2}/>
+                                <SelectOnFocusTextField label="The number of the track" variant="filled" value={trackNumber} onChange={onValueEventRun(setFilteredTrackNumber)}
+                                                        onKeyPress={onEnter(focusTrackArtist, true)} fullWidth={true} error={trackNumber !== originalTrackNumber} tabIndex={2}/>
                             </Box>
                             <Box style={{paddingLeft: "16px"}}>
                                 <Button fullWidth={true} variant="contained" onClick={decrementTrackNumber} tabIndex={1}>-</Button>
@@ -42,12 +77,12 @@ const TrackAdmin = ({number, artist, title, setNumber, setArtist, setTitle, orig
                         </Box>
                     </Grid>
                     <Grid item xs={12}>
-                        <SelectOnFocusTextField inputRef={trackArtistField} label="The artist of the track" variant="filled" value={artist} onChange={onValueEventRun(setArtist)}
-                                                onKeyPress={onEnter(focusTrackTitle, true)} fullWidth={true} error={artist !== originalArtist}/>
+                        <SelectOnFocusTextField inputRef={trackArtistField} label="The artist of the track" variant="filled" value={trackArtist} onChange={onValueEventRun(setTrackArtist)}
+                                                onKeyPress={onEnter(focusTrackTitle, true)} fullWidth={true} error={trackArtist !== originalTrackArtist}/>
                     </Grid>
                     <Grid item xs={12}>
-                        <SelectOnFocusTextField inputRef={trackTitleField} label="The title of the track" variant="filled" value={title} onChange={onValueEventRun(setTitle)}
-                                                onKeyPress={onEnter(blur, false)} fullWidth={true} error={title !== originalTitle}/>
+                        <SelectOnFocusTextField inputRef={trackTitleField} label="The title of the track" variant="filled" value={trackTitle} onChange={onValueEventRun(setTrackTitle)}
+                                                onKeyPress={onEnter(blur, false)} fullWidth={true} error={trackTitle !== originalTrackTitle}/>
                     </Grid>
                     <Grid item xs={12}>
                         <Box display="flex" alignItems="center">
