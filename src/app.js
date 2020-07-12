63925db334bae51d7a79a6bc3b4e72a84c1f230f
David ‘Bombe’ Roden <bombe@pterodactylus.net>
2020-07-12 13:12:33 +0200

    🚚 Mack backend serve compiled frontend

---
+++ src/app.js
@@ -0,0 +1,20 @@
+const express = require("express")
+const app = express()
+const port = process.env.PORT || 5001
+const path = require("path")
+const routes = require("./api/overlay/route")
+const State = require("./component/state")
+
+app.use(express.json({strict: false}))
+
+routes(app, State)
+
+// Serve any static files
+app.use(express.static(path.join(__dirname, "../frontend/build")))
+
+// Handle React routing, return all requests to React app
+app.get("*", function (request, response) {
+    response.sendFile(path.join(__dirname, "../frontend/build", "index.html"))
+})
+
+app.listen(port)
