c40da477b89ecb40ecc91a6e4a5cce12d82d4bb1
David ‘Bombe’ Roden <bombe@pterodactylus.net>
2020-09-05 23:32:27 +0200

    🎨 Add title cleaner

---
+++ src/component/title-cleaner/test/title-cleaner.test.js
@@ -0,0 +1,49 @@
+const expect = require("chai").expect
+const {titleCleaner} = require("../")
+
+describe("The Title Cleaner", () => {
+
+    it("should treat undefined values as empty strings", () => {
+        expect(titleCleaner(undefined, undefined)).to.eql({artist: "", title: ""})
+    })
+
+    it("should trim both fields", () => {
+        expect(titleCleaner("  artist  ", " title ")).to.eql({artist: "artist", title: "title"})
+    })
+
+    it("should remove excessive whitespace from both fields", () => {
+        expect(titleCleaner("  Ar   Tist  ", " Ti    Tle ")).to.eql({artist: "Ar Tist", title: "Ti Tle"})
+    })
+
+    it("should remove “original mix” from title", () => {
+        expect(titleCleaner("artist", "Title (Original Mix)")).to.eql({artist: "artist", title: "Title"})
+    })
+
+    it("should not remove a parented expression that is not a mix", () => {
+        expect(titleCleaner("artist", "Title (Closer)")).to.eql({artist: "artist", title: "Title (Closer)"})
+    })
+
+    it("should not remove a parented expression that is not a mix", () => {
+        expect(titleCleaner("artist", "Title (Closer) (Original Mix)")).to.eql({artist: "artist", title: "Title (Closer)"})
+    })
+
+    it("should remove “extended mix” from title", () => {
+        expect(titleCleaner("artist", "Title (Extended Mix)")).to.eql({artist: "artist", title: "Title"})
+    })
+
+    it("should remove “extended” from title but keep the remixer", () => {
+        expect(titleCleaner("artist", "Title (Remixer Extended Mix)")).to.eql({artist: "artist", title: "Title (Remixer Mix)"})
+    })
+
+    it("should move “feat. Somebody Else” from title to artist", () => {
+        expect(titleCleaner("Artist", "Title feat. Somebody Else (Remixer Extended Mix)")).to.eql({artist: "Artist feat. Somebody Else", title: "Title (Remixer Mix)"})
+    })
+
+    it("should move “(feat. Somebody Else)” from title to artist", () => {
+        expect(titleCleaner("Artist", "Title (feat. Somebody Else) (Remixer Extended Mix)")).to.eql({artist: "Artist feat. Somebody Else", title: "Title (Remixer Mix)"})
+    })
+
+    it("should move “ft Somebody Else” from title to artist", () => {
+        expect(titleCleaner("Artist", "Title ft Somebody Else (Remixer Extended Mix)")).to.eql({artist: "Artist feat. Somebody Else", title: "Title (Remixer Mix)"})
+    })
+})
---
+++ src/component/title-cleaner/title-cleaner.js
@@ -0,0 +1,55 @@
+const titleCleaner = (artist, title) => {
+    return [{artist: artist || "", title: title || ""}]
+        .map(splitRemixInfo)
+        .map(removeOriginalMix)
+        .map(removeExtendedMix)
+        .map(moveFeatToArtist)
+        .map(mergeRemixInfo)
+        .map(trimFields)
+        .shift()
+}
+
+const trimFields = ({artist, title}) => {
+    return {artist: artist.trim().replace(/  */g, " "), title: title.trim().replace(/  */g, " ")}
+}
+
+const splitRemixInfo = ({artist, title}) => {
+    const lastClosingParen = title.lastIndexOf(")")
+    const lastOpeningParen = title.lastIndexOf("(")
+    if ((lastOpeningParen !== -1) && (lastClosingParen !== -1)) {
+        const remixInfo = title.substring(lastOpeningParen + 1, lastClosingParen)
+        return {artist, title: title.substring(0, lastOpeningParen).trim(), remix: remixInfo}
+    }
+    return {artist, title}
+}
+
+const removeOriginalMix = ({artist, title, remix}) => {
+    if (/original mix/i.test(remix)) {
+        return {artist, title}
+    }
+    return {artist, title, remix}
+}
+
+const removeExtendedMix = ({artist, title, remix}) => {
+    if (/^extended mix$/im.test(remix)) {
+        return {artist, title}
+    }
+    if (/extended/i.test(remix)) {
+        remix = remix.split(" ").filter(e => !/extended/i.test(e)).join(" ")
+    }
+    return {artist, title, remix}
+}
+
+const moveFeatToArtist = ({artist, title, remix}) => {
+    const searchResults = /\bf(ea)?t\.? ([^)]*)/i.exec(title)
+    if (searchResults) {
+        const featuredArtist = searchResults[2]
+        const cleanedTitle = (title.substring(0, searchResults.index) + title.substring(searchResults.index + searchResults[0].length)).replace(/( \(\)|\(\) )/g, "")
+        return {artist: artist + " feat. " + featuredArtist, title: cleanedTitle, remix}
+    }
+    return {artist, title, remix}
+}
+
+const mergeRemixInfo = ({artist, title, remix}) => ({artist, title: title + (remix ? " (" + remix + ")" : "")})
+
+module.exports = {titleCleaner}
