7aa6bde146144aadbd9f70fdec990b98bd746e7b
David ‘Bombe’ Roden <bombe@pterodactylus.net>
2020-09-08 10:28:43 +0200

    🚧 Add mixcloud playlist exporter

---
+++ frontend/src/utils/playlist/mixcloud/mixcloud.js
@@ -0,0 +1,16 @@
+const moment = require("moment")
+
+const mixcloud = {
+    export: (playlistEntries, startTime) => {
+        const startMoment = moment(startTime)
+        return playlistEntries
+            .map((entry, index) => (index === 0) ? ({...entry, time: startMoment.valueOf()}) : entry)
+            .map(entry => `${formatDuration(moment.duration(moment(entry.time).diff(startMoment)))} ${entry.artist} - ${entry.title}`)
+            .join("\n")
+    }
+}
+
+const formatNumber = (number) => number.toString().padStart(2, "0")
+const formatDuration = (duration) => formatNumber(duration.hours()) + ":" + formatNumber(duration.minutes()) + ":" + formatNumber(duration.seconds())
+
+module.exports = mixcloud
