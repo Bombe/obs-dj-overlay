a325c70f4ff51035edba9f898989bcb3f37e3708
David ‘Bombe’ Roden <bombe@pterodactylus.net>
2020-09-11 20:30:15 +0200

    💄 Restyle app bar to be less intrusive

--- frontend/src/components/Admin/status/Status.js
+++ frontend/src/components/Admin/status/Status.js
@@ -1,19 +1,30 @@
 import React, {useContext, useEffect, useState} from "react"
 import AppBar from "@material-ui/core/AppBar"
 import Toolbar from "@material-ui/core/Toolbar"
-import Typography from "@material-ui/core/Typography"
+import {makeStyles} from "@material-ui/core/styles"
 
 import {SourcesContext} from "../context/sources"
 
 import styles from "./Status.module.css"
 
+const createAppBarStyles = makeStyles({
+    colorPrimary: {
+        color: "inherit",
+        backgroundColor: "inherit"
+    },
+    positionFixed: {
+        top: "auto",
+        bottom: 0
+    }
+})
+
 const Status = () => {
 
+    const appBarStyle = createAppBarStyles()
     const sources = useContext(SourcesContext)
     const [hidden, setHidden] = useState(!sources.loaded)
 
     const traktorConnected = sources.traktor && (sources.traktor.length !== 0)
-    const errorDetected = !traktorConnected
 
     useEffect(() => {
         if (sources.loaded) {
@@ -23,9 +34,9 @@ const Status = () => {
 
     return (
         <>
-            <AppBar position="fixed" style={{top: "auto", bottom: 0, visibility: hidden ? "hidden" : "inherit"}}>
-                <Toolbar variant="dense" className={errorDetected ? styles.Error : styles.Okay} style={{minHeight: "inherit", paddingTop: "0.5ex", paddingBottom: "0.5ex"}}>
-                    <Typography className={traktorConnected ? styles.Online : styles.Offline}>Traktor</Typography>
+            <AppBar position="fixed" classes={appBarStyle} style={{visibility: hidden ? "hidden" : "inherit"}}>
+                <Toolbar variant="dense" className={styles.Toolbar}>
+                    <div className={[styles.Traktor, traktorConnected ? styles.Online : styles.Offline].join(" ")}>Traktor</div>
                 </Toolbar>
             </AppBar>
             <Toolbar variant="dense"/>
