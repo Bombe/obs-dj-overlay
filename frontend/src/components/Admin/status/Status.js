2171c5f8c1b704f1b405099f1839eef7023206b1
David ‘Bombe’ Roden <bombe@pterodactylus.net>
2020-09-11 20:30:48 +0200

    💄 Add version information to app bar

--- frontend/src/components/Admin/status/Status.js
+++ frontend/src/components/Admin/status/Status.js
@@ -4,6 +4,7 @@ import Toolbar from "@material-ui/core/Toolbar"
 import {makeStyles} from "@material-ui/core/styles"
 
 import {SourcesContext} from "../context/sources"
+import RuntimeService from "../../../services/runtime"
 
 import styles from "./Status.module.css"
 
@@ -23,6 +24,7 @@ const Status = () => {
     const appBarStyle = createAppBarStyles()
     const sources = useContext(SourcesContext)
     const [hidden, setHidden] = useState(!sources.loaded)
+    const [runtimeVersion, setRuntimeVersion] = useState()
 
     const traktorConnected = sources.traktor && (sources.traktor.length !== 0)
 
@@ -32,17 +34,29 @@ const Status = () => {
         }
     }, [sources])
 
+    useEffect(() => {
+        RuntimeService.get()
+            .then(runtime => setRuntimeVersion(runtime.version))
+    }, [])
+
     return (
         <>
             <AppBar position="fixed" classes={appBarStyle} style={{visibility: hidden ? "hidden" : "inherit"}}>
                 <Toolbar variant="dense" className={styles.Toolbar}>
                     <div className={[styles.Traktor, traktorConnected ? styles.Online : styles.Offline].join(" ")}>Traktor</div>
+                    {runtimeVersion && <div className={styles.Version}>
+                        <a href="https://github.com/Bombe/obs-dj-overlay">obs-dj-overlay</a> <VersionInformation hash={runtimeVersion.hash} name={runtimeVersion.name}/>
+                    </div>}
                 </Toolbar>
             </AppBar>
             <Toolbar variant="dense"/>
         </>
     )
 
+}
+
+const VersionInformation = ({hash, name}) => {
+    return <span title={hash}>{name}</span>
 }
 
 export {Status as default}
