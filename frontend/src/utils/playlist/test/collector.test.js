e26aec31562aaaedf7fa2e7ca4389d0b6659af55
David ‘Bombe’ Roden <bombe@pterodactylus.net>
2023-08-28 22:37:18 +0200

    ♻️ Rename variable and test

--- frontend/src/utils/playlist/test/collector.test.js
+++ frontend/src/utils/playlist/test/collector.test.js
@@ -1,5 +1,5 @@
 const {expect} = require("chai")
-const mixcloud = require("../")
+const collector = require("../")
 
 const entry1 = {artist: "Artist 1", title: "Title 1", time: 1599551262000} // Di  8 Sep 2020 09:47:42 CEST
 const entry2 = {artist: "Artist 2", title: "Title 2", time: 1599551562000} // Di  8 Sep 2020 09:52:42 CEST
@@ -8,29 +8,29 @@ const entry4 = {artist: "Artist 4", title: "Title 4", time: 1599553062000} // Di
 const entry5 = {artist: "Artist 5", title: "Title 5", time: 1599556062000} // Di  8 Sep 2020 11:07:42 CEST
 const historyEntries = [entry1, entry2, entry3, entry4, entry5]
 
-describe("The Mixcloud Exporter", () => {
+describe("The Export Collector", () => {
 
     it("should collect nothing for empty history", () => {
-        expect(mixcloud.collect(historyEntries)).to.eql([])
+        expect(collector.collect(historyEntries)).to.eql([])
     })
 
     it("should collect nothing if range in invalid", () => {
-        expect(mixcloud.collect(historyEntries, undefined, undefined)).to.eql([])
+        expect(collector.collect(historyEntries, undefined, undefined)).to.eql([])
     })
 
     it("should collect nothing if to is before from", () => {
-        expect(mixcloud.collect(historyEntries, "20200908T1100", "20200908T1000")).to.eql([])
+        expect(collector.collect(historyEntries, "20200908T1100", "20200908T1000")).to.eql([])
     })
 
     it("should include the first track if before is earlier than the first track", () => {
-        expect(mixcloud.collect(historyEntries, "20200908T0900", "20200908T1200")).to.eql(historyEntries)
+        expect(collector.collect(historyEntries, "20200908T0900", "20200908T1200")).to.eql(historyEntries)
     })
 
     it("should include the second track if before is later than the second track", () => {
-        expect(mixcloud.collect(historyEntries, "20200908T1000", "20200908T1200")).to.eql([entry2, entry3, entry4, entry5])
+        expect(collector.collect(historyEntries, "20200908T1000", "20200908T1200")).to.eql([entry2, entry3, entry4, entry5])
     })
 
     it("should not include the last track if to is earlier", () => {
-        expect(mixcloud.collect(historyEntries, "20200908T1000", "20200908T1100")).to.eql([entry2, entry3, entry4])
+        expect(collector.collect(historyEntries, "20200908T1000", "20200908T1100")).to.eql([entry2, entry3, entry4])
     })
 })
