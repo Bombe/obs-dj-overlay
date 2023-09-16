46aee722baa556945891279a39413d4c98a3a60c
David ‘Bombe’ Roden <bombe@pterodactylus.net>
2023-09-16 16:39:30 +0200

    ✅ Add test for TrackInfo element

--- frontend/src/components/TrackInfo/TrackInfo.js
+++ frontend/src/components/TrackInfo/TrackInfo.js
@@ -8,15 +8,15 @@ const TrackInfo = () => {
     return (overlayInfo.track && (overlayInfo.track.artist || overlayInfo.track.title)) ? (
         <div className="TrackInfo">
             {overlayInfo.track.number ?
-                <div className="Number">{overlayInfo.track.number}</div>
+                <div className="Number" title="track-number">{overlayInfo.track.number}</div>
                 : <></>
             }
-            <div className="Artist">{overlayInfo.track.artist}</div>
-            <div className="Title">{overlayInfo.track.title}</div>
+            <div className="Artist" title="artist">{overlayInfo.track.artist}</div>
+            <div className="Title" title="title">{overlayInfo.track.title}</div>
             {overlayInfo.lastTrack.artist !== "" && overlayInfo.lastTrack.title !== "" ?
                 <div className="LastTrack">
-                    <div className="LastArtist">{overlayInfo.lastTrack.artist}</div>
-                    <div className="LastTitle">{overlayInfo.lastTrack.title}</div>
+                    <div className="LastArtist" title="last-artist">{overlayInfo.lastTrack.artist}</div>
+                    <div className="LastTitle" title="last-title">{overlayInfo.lastTrack.title}</div>
                 </div>
                 : <></>
             }
---
+++ frontend/src/components/TrackInfo/TrackInfo.test.js
@@ -0,0 +1,58 @@
+import {render, screen} from "@testing-library/react"
+import React from 'react'
+import {expect} from 'chai'
+
+import TrackInfo from '.'
+import WithOverlayInfo from '../OverlayInfoContext';
+
+describe('The Track Info element', () => {
+
+    it('should not show anything if no track is defined', () => {
+        render(<WithOverlayInfo overlayInfo={{}}><TrackInfo/></WithOverlayInfo>)
+        expect(screen.queryByTitle('artist')).to.be.null
+    });
+
+    it('should not show the track number if no track number is set', () => {
+        render(<WithOverlayInfo overlayInfo={{track: {artist: 'Artist'}, lastTrack: {}}}><TrackInfo/></WithOverlayInfo>)
+        expect(screen.queryByTitle('track-number')).to.be.null
+    });
+
+    it('should not show the track number if track number is zero', () => {
+        render(<WithOverlayInfo overlayInfo={{track: { number: 0, artist: 'Artist'}, lastTrack: {}}}><TrackInfo/></WithOverlayInfo>)
+        expect(screen.queryByTitle('track-number')).to.be.null
+    });
+
+    it('should show the track number if track number is set', () => {
+        render(<WithOverlayInfo overlayInfo={{track: {number: 12, artist: 'Artist'}, lastTrack: {}}}><TrackInfo/></WithOverlayInfo>)
+        expect(screen.getByTitle('track-number').textContent).to.eql('12')
+    });
+
+    it('should show the artist if a track is set', () => {
+        render(<WithOverlayInfo overlayInfo={{track: {artist: 'Artist'}, lastTrack: {}}}><TrackInfo/></WithOverlayInfo>)
+        expect(screen.getByTitle('artist').textContent).to.eql('Artist')
+    });
+
+    it('should show the title if a track is set', () => {
+        render(<WithOverlayInfo overlayInfo={{track: {title: 'Title'}, lastTrack: {}}}><TrackInfo/></WithOverlayInfo>)
+        expect(screen.getByTitle('title').textContent).to.eql('Title')
+    });
+
+    it('should not display last artist if last artist is not set', () => {
+        render(<WithOverlayInfo overlayInfo={{track: {artist: 'Artist'}, lastTrack: {artist: '', title: 'Last Title'}}}><TrackInfo/></WithOverlayInfo>)
+        expect(screen.queryByTitle('last-artist')).to.be.null
+        expect(screen.queryByTitle('last-title')).to.be.null
+    });
+
+    it('should not display last artist if last title is not set', () => {
+        render(<WithOverlayInfo overlayInfo={{track: {artist: 'Artist'}, lastTrack: {artist: 'Last Artist', title: ''}}}><TrackInfo/></WithOverlayInfo>)
+        expect(screen.queryByTitle('last-artist')).to.be.null
+        expect(screen.queryByTitle('last-title')).to.be.null
+    });
+
+    it('should display last artist and title if last artist and title are set', () => {
+        render(<WithOverlayInfo overlayInfo={{track: {artist: 'Artist'}, lastTrack: {artist: 'Last Artist', title: 'Last Title'}}}><TrackInfo/></WithOverlayInfo>)
+        expect(screen.getByTitle('last-artist').textContent).to.eql('Last Artist')
+        expect(screen.getByTitle('last-title').textContent).to.eq('Last Title')
+    });
+
+});
