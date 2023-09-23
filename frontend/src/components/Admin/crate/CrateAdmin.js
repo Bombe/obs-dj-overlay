b231cbe26a38c5adc35e613c8d0e73c81506baad
David ‘Bombe’ Roden <bombe@pterodactylus.net>
2023-09-23 19:02:18 +0200

    💄 Use “filled” variant for input field

--- frontend/src/components/Admin/crate/CrateAdmin.js
+++ frontend/src/components/Admin/crate/CrateAdmin.js
@@ -92,7 +92,7 @@ const CrateAdmin = ({setArtist, setTitle, setCover, scrollToTrack}) => {
             </Grid>
             <Grid item xs={12}>
                 <Box display="flex" alignItems='center'>
-                    <Box flexGrow={1}><TextField id="import-string" label="Import" fullWidth={true} value={importString} onChange={onValueEventRun(setImportString)}></TextField></Box>
+                    <Box flexGrow={1}><TextField id="import-string" label="Import" variant='filled' fullWidth={true} value={importString} onChange={onValueEventRun(setImportString)}/></Box>
                     <Box paddingLeft="16px"><Button onClick={importFromInputField} variant="contained" startIcon={<PlaylistAdd/>}>Import</Button></Box>
                     <Box paddingLeft="16px"><Button onClick={reloadCrate} variant="contained" startIcon={<Refresh/>}>Reload</Button></Box>
                     <Box paddingLeft="16px"><Button onClick={clearCrate} variant="contained" startIcon={<Delete/>}>Reset</Button></Box>
