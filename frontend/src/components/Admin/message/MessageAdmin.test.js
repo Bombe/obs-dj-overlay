e10c478fcf7ad45c58c88fbf8d4fdeba7cba55f8
David ‘Bombe’ Roden <bombe@pterodactylus.net>
2023-10-22 00:43:45 +0200

    ✅ Add test for message admin component
    Also, switch it over to using the overlay service context, exchange the
    deprecated onKeyPress event for onKeyUp, and fix some React state messiness.

---
+++ frontend/src/components/Admin/message/MessageAdmin.test.js
@@ -0,0 +1,74 @@
+import {act, render, screen} from '@testing-library/react'
+import {expect} from 'chai'
+
+import {MessageAdmin} from './MessageAdmin'
+import WithOverlayService from '../../../contexts/overlayService'
+import {doNothing, user} from '../../../utils/tests'
+import userEvent from '@testing-library/user-event'
+
+describe('The Message Section', () => {
+
+    const defaultOverlayService = {
+        get: () => Promise.resolve({message: ''}),
+        setMessage: doNothing
+    }
+
+    it('should have an input field', async () => {
+        await act(async () => render(<WithOverlayService overlayService={defaultOverlayService}><MessageAdmin/></WithOverlayService>))
+        expect(screen.getByLabelText(/message/i)).to.exist
+    })
+
+    it('should get initial message from overlay service context', async () => {
+        const overlayService = {...defaultOverlayService, get: () => Promise.resolve({message: 'Test Message'})}
+        await act(async () => render(<WithOverlayService overlayService={overlayService}><MessageAdmin/></WithOverlayService>))
+        expect(screen.getByLabelText(/message/i).value).to.eql('Test Message')
+    })
+
+    it('should have a button to send message', async () => {
+        await act(async () => render(<WithOverlayService overlayService={defaultOverlayService}><MessageAdmin/></WithOverlayService>))
+        expect(screen.getByRole('button', {name: /update/i})).to.exist
+    })
+
+    it('should send message to overlay service when button is pressed', async () => {
+        let sentMessage
+        const overlayService = {...defaultOverlayService, setMessage: message => sentMessage = message}
+        render(<WithOverlayService overlayService={overlayService}><MessageAdmin/></WithOverlayService>)
+        await user.type(screen.getByLabelText(/message/i), 'test message')
+        await user.click(screen.getByRole('button', {name: /update/i}))
+        expect(sentMessage).to.eql('test message')
+    })
+
+    it('should send message to overlay service when ctrl-enter is pressed', async () => {
+        let sentMessage
+        const overlayService = {...defaultOverlayService, setMessage: message => sentMessage = message}
+        render(<WithOverlayService overlayService={overlayService}><MessageAdmin/></WithOverlayService>)
+        await user.type(screen.getByLabelText(/message/i), 'test message{Control>}{Enter}{/ControlLeft}')
+        expect(sentMessage).to.eql('test message')
+    })
+
+    it('should have a button to clear the message', async () => {
+        await act(async () => render(<WithOverlayService overlayService={defaultOverlayService}><MessageAdmin/></WithOverlayService>))
+        expect(screen.getByRole('button', {name: /clear/i})).to.exist
+    })
+
+    it('should clear the input field when clear button is pressed', async () => {
+        render(<WithOverlayService overlayService={defaultOverlayService}><MessageAdmin/></WithOverlayService>)
+        await user.type(screen.getByLabelText(/message/i), 'test message')
+        await user.click(screen.getByRole('button', {name: /clear/i}))
+        expect(screen.getByLabelText(/message/i).value).to.eql('')
+    })
+
+    it('should have a button to restore the message', async () => {
+        await act(async () => render(<WithOverlayService overlayService={defaultOverlayService}><MessageAdmin/></WithOverlayService>))
+        expect(screen.getByRole('button', {name: /restore/i})).to.exist
+    })
+
+    it('should restore the message when the restore button is pressed', async () => {
+        const overlayService = {...defaultOverlayService, get: () => Promise.resolve({message: 'Initial Message'})}
+        render(<WithOverlayService overlayService={overlayService}><MessageAdmin/></WithOverlayService>)
+        await user.type(screen.getByLabelText(/message/i), 'test message')
+        await user.click(screen.getByRole('button', {name: /restore/i}))
+        expect(screen.getByLabelText(/message/i).value).to.eql('Initial Message')
+    })
+
+})
