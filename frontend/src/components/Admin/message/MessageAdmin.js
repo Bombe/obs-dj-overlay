7440181a5f198d19207b333a34698f6d65e78b0e
David ‘Bombe’ Roden <bombe@pterodactylus.net>
2023-10-21 18:13:42 +0200

    🚨 Fix deprecated attribute

--- frontend/src/components/Admin/message/MessageAdmin.js
+++ frontend/src/components/Admin/message/MessageAdmin.js
@@ -52,7 +52,7 @@ const MessageAdmin = () => {
             <Grid container spacing={2} direction="column" alignItems="stretch">
                 <Grid item xs={12}>
                     <SelectOnFocusTextField label="A message to display" variant="filled" value={message} onChange={onValueEventRun(setMessage)} onKeyPress={textAreaEnterHandler(sendMessage)}
-                                            fullWidth={true} multiline={true} rows={6} helperText="Press Ctrl-Enter to submit!" error={message !== originalMessage}/>
+                                            fullWidth={true} multiline={true} minRows={6} helperText="Press Ctrl-Enter to submit!" error={message !== originalMessage}/>
                 </Grid>
                 <Grid item xs={12}>
                     <Box display="flex" alignItems="center">
