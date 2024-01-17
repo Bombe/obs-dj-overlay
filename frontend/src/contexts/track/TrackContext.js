55635511bb9c443db38c2371bd8eb39183e143ec
David ‘Bombe’ Roden <bombe@pterodactylus.net>
2024-01-17 17:42:57 +0100

    🚸 Remove “featuring” from track name when it’s in parens, too

--- frontend/src/contexts/track/TrackContext.js
+++ frontend/src/contexts/track/TrackContext.js
@@ -11,11 +11,11 @@ const cleanMixName = mixName => {
 }
 
 const removeFeaturing = title =>
-    title.split(/ feat\./)[0]
+    title.split(/(^| )feat\./)[0]
 
 const cleanTitle = title => {
     const trackParts = title.split(/\)? \(|\)$/)
-    return removeFeaturing(trackParts.at(0)) + trackParts.slice(1).map(cleanMixName).filter(n => n !== undefined).filter(n => n !== '').map(s => ` (${s})`).join('')
+    return removeFeaturing(trackParts.at(0)) + trackParts.slice(1).map(removeFeaturing).map(cleanMixName).filter(n => n !== undefined).filter(n => n !== '').map(s => ` (${s})`).join('')
 }
 
 const TrackContext = createContext(undefined)
