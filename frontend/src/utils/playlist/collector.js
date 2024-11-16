bde314dc6076b4a004e2ed40e87a19ef48350883
David ‘Bombe’ Roden <bombe@pterodactylus.net>
2024-11-16 19:51:18 +0100

    🚧 Add “strict first track” setting to playlist collector

--- frontend/src/utils/playlist/collector.js
+++ frontend/src/utils/playlist/collector.js
@@ -1,13 +1,13 @@
 const moment = require("moment")
 
 const collector = {
-    collect: (historyEntries, from, to) => {
+    collect: (historyEntries, from, to, strictFirstTrack) => {
         if (((from === undefined) || (to === undefined)) || moment(to).isBefore(moment(from))) {
             return []
         }
         const entriesInRange = historyEntries.filter(entry => moment(entry.time).isBetween(moment(from), moment(to), "seconds", "[]"))
         const entriesBeforeRange = historyEntries.filter(entry => moment(entry.time).isBefore(moment(from)))
-        if (entriesBeforeRange.length !== 0) {
+        if ((entriesBeforeRange.length !== 0) && !strictFirstTrack) {
             entriesInRange.unshift(entriesBeforeRange.pop())
         }
         return entriesInRange
