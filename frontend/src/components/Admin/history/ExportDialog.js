7082521199bd51153620164883e3310338473bf4
David ‘Bombe’ Roden <bombe@pterodactylus.net>
2024-11-16 20:08:30 +0100

    ✨ Add checkbox to restrict playlist export to selected time

--- frontend/src/components/Admin/history/ExportDialog.js
+++ frontend/src/components/Admin/history/ExportDialog.js
@@ -1,5 +1,5 @@
 import React, { useCallback, useEffect, useState } from 'react'
-import { Button, Dialog, DialogActions, DialogContent, DialogTitle, Grid2 as Grid, TextField } from '@mui/material'
+import { Button, Checkbox, Dialog, DialogActions, DialogContent, DialogTitle, FormControlLabel, Grid2 as Grid, TextField } from '@mui/material'
 import { Assignment } from '@mui/icons-material'
 import {CopyToClipboard} from "react-copy-to-clipboard/lib/Component"
 
@@ -9,10 +9,11 @@ import mixcloud from "../../../utils/playlist/mixcloud"
 
 import styles from "./HistoryAdmin.module.css"
 
-const ExportDialog = ({open, setOpened, historyEntries}) => {
+const ExportDialog = ({open, setOpened, historyEntries, onCopy = (text) => {}}) => {
 
     const [from, setFrom] = useState("")
     const [to, setTo] = useState("")
+    const [restrictTracks, setRestrictTracks] = useState(false)
     const [playlistEntries, setPlaylistEntries] = useState([])
     const [htmlTablePlaylist, setHtmlTablePlaylist] = useState("")
     const [mixcloudPlaylist, setMixcloudPlaylist] = useState("")
@@ -29,9 +30,13 @@ const ExportDialog = ({open, setOpened, historyEntries}) => {
         setTo(target.value)
     }, [setTo])
 
+    const toggleRestrictTracks = useCallback(() => {
+        setRestrictTracks(!restrictTracks)
+    }, [restrictTracks, setRestrictTracks])
+
     useEffect(() => {
-        setPlaylistEntries(playlist.collect(historyEntries, from, to))
-    }, [historyEntries, from, to])
+        setPlaylistEntries(playlist.collect(historyEntries, from, to, restrictTracks))
+    }, [historyEntries, from, to, restrictTracks])
 
     useEffect(() => {
         setHtmlTablePlaylist(htmlTable.export(playlistEntries, from))
@@ -48,13 +53,14 @@ const ExportDialog = ({open, setOpened, historyEntries}) => {
                 <Grid container direction="column" spacing={2}>
                     <Grid paddingTop={1}><TextField label="From" onChange={changeFrom} type="datetime-local" value={from} slotProps={{ htmlInput: { step: 1, role: 'textbox' }, inputLabel: { shrink: true } }}/></Grid>
                     <Grid><TextField label="To" onChange={changeTo} type="datetime-local" value={to} slotProps={{ htmlInput: { step: 1, role: 'textbox' }, inputLabel: { shrink: true } }}/></Grid>
+                    <FormControlLabel label="Restrict to Time Selection" control={<Checkbox checked={restrictTracks} onChange={toggleRestrictTracks}/>}/>
                 </Grid>
             </DialogContent>
             <DialogActions className={styles.ExportDialog}>
-                <CopyToClipboard text={htmlTablePlaylist}>
+                <CopyToClipboard text={htmlTablePlaylist} onCopy={() => onCopy(htmlTablePlaylist)}>
                     <Button disabled={from === "" || to === ""} variant="contained" color="greys" startIcon={<Assignment/>}>HTML Table</Button>
                 </CopyToClipboard>
-                <CopyToClipboard text={mixcloudPlaylist}>
+                <CopyToClipboard text={mixcloudPlaylist} onCopy={() => onCopy(mixcloudPlaylist)}>
                     <Button disabled={from === "" || to === ""} variant="contained" color="greys" startIcon={<Assignment/>}>Mixcloud</Button>
                 </CopyToClipboard>
             </DialogActions>
